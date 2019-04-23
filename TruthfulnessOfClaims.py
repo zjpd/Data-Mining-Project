6277a42ba65061c4fc8e020a8637229d49768d38
zjpdasdf <jin.zhang.liverpool@gmail.com>
2019-04-24 00:13:20 +0100

    update to the nn model

--- TruthfulnessOfClaims.py
+++ TruthfulnessOfClaims.py
@@ -18,8 +18,54 @@ import collections
 import math
 import datetime
 import numpy as np
-import pandas as pd
 import json
-import csv
 from numpy import array, newaxis
+
+
+'''
+	1. get train data that includes claim in train.jsonl
+	2. get the evidences of the claim and retrieve the relevant sentences
+	3. label the sentences
+	4. setup the model
+	
+'''
+
+train_path = 'D://document//UCL//Data Mining//data'
+data = []
+with open(train_path+'//train.jsonl', 'r') as file:
+	lines = file.readlines()
+	for line in lines:
+		tmp = json.loads(line)
+		if tmp['label'] != 'NOT ENOUGH INFO' :
+			data.append(tmp)
+
+			
+evidences = []
+for line in data:
+	tmp = {}
+	tmpstr = str(line['evidence'])
+	tmpstr = tmpstr.replace('[','')
+	tmpstr = tmpstr.replace(']','')
+	tmpstr = list(eval(tmpstr))
+	
+	tmplist = []
+	n = 0
+	while n < len(tmpstr):
+		if n+4 > len(tmpstr):
+			break
+		
+		tmplist.append(tmpstr[n+2: n+4])
+		n += 4
+	
+	tmp[line['id']] = tmplist
+	evidences.append(tmp)
+
+
+x_train = []
+y_train = []
+
+
+
+def get_train_data():
+
 
